373d2d59eeb60d58b20e347338ea2cbead0854e8
Mizuki Matsutani <mizuki2064matsutani@gmail.com>
2020-10-01 17:42:16 +0900

    サンプルテストコード追加

---
+++ src/tests/Feature/ApiTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    public function APIをテストする()
+    {
+        $response = $this->json('get', '/api/test?param=bbb');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'aaa' => 'bbb',
+            ]);
+    }
+}
---
+++ src/tests/Unit/DatabaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    public function DBのテスト()
+    {
+        User::create($data = [
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $this->assertDatabaseHas('users', $data);
+    }
+}
